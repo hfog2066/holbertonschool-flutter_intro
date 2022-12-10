dffb5bc39e631f77928ca820b079061ca1acada1
hfog2066 <hfog2000@yahoo.com>
2022-12-09 22:07:21 -0500

    0x01. Flutter - Flutter Intro

---
+++ lib/models.dart
@@ -0,0 +1,16 @@
+class Character {
+  late String name;
+  late String img;
+  late int id;
+  Character(this.name, this.img, this.id);
+
+
+Character.fromJson(Map<String, dynamic> json) {
+  Character(
+    name = json["name"],
+    img = json["img"],
+    id = json["id"],
+  );
+ }
+
+}
